5adb8cd522160e718fa51c8dc210638bbb6496a4
iScarcy <scarcella.giuseppe@gmail.com>
2024-02-12 23:49:16 +0100

    first commit

---
+++ RecurringEvents.js
@@ -0,0 +1,19 @@
+const events = [
+    {
+        "type":"Compleanno",
+        "data":"1983-08-22",
+        "description":"Giuseppe Scarcella"
+    },
+    {
+        "type":"Compleanno",
+        "data":"1979-02-10",
+        "description":"Salvatore Alizio"
+    },
+    {
+        "type":"Onomastico",
+        "data":"2024-04-19",
+        "description":"Giuseppe Scarcella"
+    }
+];
+
+module.exports = {events}
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const rc = require('./RecurringEvents');
+var app = express();
+
+app.get("/",(req,res) => {
+    res.status(200).json(rc.events);
+});
+
+app.get("/compleanni",(req,res) => {
+    let birthdays = [...rc.events];
+    birthdays = birthdays.filter((birthday)=>{
+            return birthday.type == "Compleanno"
+    })
+    res.status(200).json(birthdays);
+});
+
+
+app.get("/onomastici",(req,res) => {
+    let birthdays = [...rc.events];
+    birthdays = birthdays.filter((birthday)=>{
+            return birthday.type == "Onomastico"
+    })
+    res.status(200).json(birthdays);
+});
+
+app.get("/eventi",(req,res) => {
+    
+    const query = req.query;
+    
+    let eventi = [...rc.events];
+
+    var oggiDT = new Date(query.oggi);
+    var mese = oggiDT.getMonth()+1;
+    var day = oggiDT.getDate();
+    
+    if(query){
+    
+        eventi = eventi.filter((evento) => {
+        
+        var dateDT = new Date(evento.data);
+        var evento_mese     = dateDT.getMonth()+1;
+        var evento_giorno = dateDT.getDate();
+        if(evento_giorno == day && evento_mese == mese)
+        {
+          return evento.data;
+        } 
+            
+        });
+    }
+
+    res.status(200).json(eventi);
+});
+
+app.listen(3000);
